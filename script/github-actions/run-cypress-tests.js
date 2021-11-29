5818f261abf5a86b7c5cb96f01c3b68db97f636a
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-11-29 11:33:25 -0600

    WIP

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -1,23 +1,29 @@
 const { runCommandSync } = require('../utils');
 const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const { integrationFolder, testFiles } = require('../../config/cypress.json');
 
-const tests = JSON.parse(process.env.TESTS);
-const step = Number(process.env.STEP);
-const numContainers = Number(process.env.NUM_CONTAINERS);
+const pattern = path.join(__dirname, '../..', integrationFolder, testFiles);
+const tests = glob.sync(pattern);
+
+// const tests = JSON.parse(process.env.TESTS);
+const step = 1;
+const numContainers = 8;
 const divider = Math.ceil(tests.length / numContainers);
 
 const batch = tests
-  .map(test =>
-    test.replace('/home/runner/work/vets-website/vets-website', '../..'),
-  )
+  // .map(test =>
+  //   test.replace('/home/runner/work/vets-website/vets-website', '../..'),
+  // )
   .slice(step * divider, (step + 1) * divider)
-  .join(`');require('`);
+  .join(`');})();(function() {require('`);
 
 runCommandSync(`mkdir -p src/tests/`);
 runCommandSync(`touch src/tests/merged-cypress-tests.cypress.spec.js`);
 
 const fileText = `describe('Batch ${step}', () => {
-  require('${batch}');
+  (function() {require('${batch}');})();
 });`;
 
 fs.writeFileSync('src/tests/merged-cypress-tests.cypress.spec.js', fileText);
---
+++ src/tests/merged-cypress-tests.cypress.spec.js
@@ -0,0 +1,102 @@
+/* eslint-disable import/no-absolute-path */
+describe('Batch 1', () => {
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/happy-path/current.happy.path.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/happy-path/everything.happy.path.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/multiple-appointments/already.checked.in.appointment.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/multiple-appointments/appointments.are.sorted.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/multiple-appointments/bad.formatted.datetime.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/multiple-appointments/posting.appointment.data.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/multiple-appointments/veterans.may.refresh.their.appointments.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/session/no.data.in.redux.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/session/no.session.to.reload.on.refresh.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/session/session.is.saved.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/session/session.reloads.on.refresh.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-3-multiple-appointments/session/url.is.priotized.over.session.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-4-demographics/demographics-page/demographics.display.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-4-demographics/feature-toggle/demographics.disabled.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-4-demographics/feature-toggle/demographics.enabled.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-4-demographics/happy-path/current.happy.path.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-4-demographics/happy-path/everything.happy.path.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-5-next-of-kin/feature-toggle/next-of-kin.disabled.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-5-next-of-kin/feature-toggle/next-of-kin.enabled.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-5-next-of-kin/happy-path/current.happy.path.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-5-next-of-kin/happy-path/everything.happy.path.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-5-next-of-kin/next-of-kin-page/next-of-kin.display.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-5-next-of-kin/SeeStaff-page/SeeStaff.back.link.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/day-of/tests/e2e/phase-5-next-of-kin/SeeStaff-page/SeeStaff.display.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/pre-check-in/tests/e2e/app-enabled/disabled.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/pre-check-in/tests/e2e/app-enabled/enabled.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/check-in/pre-check-in/tests/e2e/happy-path/happy.path.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/claims-status/tests/e2e/00.claims-list-breadcrumb.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/claims-status/tests/e2e/00.claims-list-empty.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/claims-status/tests/e2e/00.claims-list.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/claims-status/tests/e2e/01.claim-status-decision.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/claims-status/tests/e2e/01.claim-status-est-current.cypress.spec.js');
+  })();
+  (function() {
+    require('/Users/dariuszdzien/code/vets-website/src/applications/claims-status/tests/e2e/01.claim-status-est-past.cypress.spec.js');
+  })();
+});
